97ec4f60257b272d5839582538c0c1e04557c458
Jason <jwrounds@usfca.edu>
2021-04-17 14:54:34 -0500

    completed part 3

--- src/app/crew/crew.component.ts
+++ src/app/crew/crew.component.ts
@@ -7,17 +7,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CrewComponent implements OnInit {
 
-  inCrew: boolean = false;
   crew: object[] = [];
+  hover: object = null;
 
   candidates: object[] = [
-    {name: "Sally Ride", photo: 'https://handlers.education.launchcode.org/static/images/sally-ride.jpg'},
-    {name: "Mae Jemison", photo: 'https://handlers.education.launchcode.org/static/images/mae-jemison.jpg'},
-    {name: "Ellen Ochoa", photo: 'https://handlers.education.launchcode.org/static/images/ellen-ochoa.jpg'},
-    {name: "Frederick Gregory", photo: 'https://handlers.education.launchcode.org/static/images/frederick-gregory.jpg'},
-    {name: "Guion Bluford", photo: 'https://handlers.education.launchcode.org/static/images/guion-bluford.jpg'},
-    {name: "Kjell Lindgren", photo: 'https://handlers.education.launchcode.org/static/images/kjell-lindgren.jpg'},
-    {name: "Jeanette Epps", photo: 'https://handlers.education.launchcode.org/static/images/jeanette-epps.jpg'}
+    {name: "Sally Ride", inCrew: false, photo: 'https://handlers.education.launchcode.org/static/images/sally-ride.jpg'},
+    {name: "Mae Jemison", inCrew: false, photo: 'https://handlers.education.launchcode.org/static/images/mae-jemison.jpg'},
+    {name: "Ellen Ochoa", inCrew: false, photo: 'https://handlers.education.launchcode.org/static/images/ellen-ochoa.jpg'},
+    {name: "Frederick Gregory", inCrew: false, photo: 'https://handlers.education.launchcode.org/static/images/frederick-gregory.jpg'},
+    {name: "Guion Bluford", inCrew: false, photo: 'https://handlers.education.launchcode.org/static/images/guion-bluford.jpg'},
+    {name: "Kjell Lindgren", inCrew: false, photo: 'https://handlers.education.launchcode.org/static/images/kjell-lindgren.jpg'},
+    {name: "Jeanette Epps", inCrew: false, photo: 'https://handlers.education.launchcode.org/static/images/jeanette-epps.jpg'}
   ];
 
   constructor() { }
@@ -25,5 +25,14 @@ export class CrewComponent implements OnInit {
   ngOnInit() { }
 
   // Code the 'addCrewMember' function here:
-
+  addCrewMember(member) {
+    if (!member.inCrew && this.crew.length < 3) {
+      this.crew.push(member);
+      member.inCrew = true;
+    } else if (member.inCrew) {
+      let index = this.crew.indexOf(member);
+      this.crew.splice(index, 1);
+      member.inCrew = false;
+    }
+  }
 }
